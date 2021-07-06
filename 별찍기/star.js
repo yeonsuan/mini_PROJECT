4b4ee880c3c89f284d43e5aca3b115d71bf91225
yeonsuan <sweetsuan@naver.com>
2021-07-06 15:17:44 +0900

    기본별찍기

---
+++ 별찍기/star.js
@@ -0,0 +1,13 @@
+//별찍기
+
+for (i=0; i<5; i++){
+    console.log('*'.repeat(i+1));
+}
+
+for(i=5; i>0; i--){
+    console.log('*'.repeat(i-1));
+}
+
+for(i=0; i<5; i++){
+    console.log('*'.repeat(5-i));
+}
